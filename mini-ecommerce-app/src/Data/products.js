6f933e3ca650e9f53f0954aa592bd8d33371f162
guptamayank9 <guptamayank242032@gmail.com>
2026-05-03 14:06:02 +0530

    add product data

--- mini-ecommerce-app/src/Data/products.js
+++ mini-ecommerce-app/src/Data/products.js
@@ -1,7 +1,25 @@
 const products = [
-  { id: 1, name: "Shoes", price: 1000 },
-  { id: 2, name: "Shirt", price: 800 },
-  { id: 3, name: "Watch", price: 1500 },
+  {
+    id: 1,
+    name: "Men Shoes",
+    price: 1200,
+    category: "men",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "Women Dress",
+    price: 1500,
+    category: "women",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 3,
+    name: "Men Shirt",
+    price: 800,
+    category: "men",
+    image: "https://via.placeholder.com/150"
+  },
 ];
 
 export default products;
