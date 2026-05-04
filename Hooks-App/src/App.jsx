fd1b59ef6be09fb202a2512848bfd3dde1110d6e
guptamayank9 <guptamayank242032@gmail.com>
2026-05-04 17:59:54 +0530

    add hooks usestate,useeffect,useref

--- Hooks-App/src/App.jsx
+++ Hooks-App/src/App.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 
 const App = () => {
+
+  const[count ,setCount] = useState(0);
+
+  useEffect(()=>{
+    console.log("component loaded");
+  },[]);///[] = sirf ek baar chalega
+
+
+
+  const inputRef = useRef();////direct DOM access  /////. focus → input pe cursor   =>>>button click → cursor input me aaya? → next
+
+
   return (
-    <div>
-     
+    <>
+    <div className='flex bg-yellow-400 w-140 h-50 grid text-2xl justify-center items-center  m-20'>
+     <h1 className='p-8'>Count :{count}</h1>
+     <button className='bg-gray-500 hover:bg-red-700 border-none focus:outline-2 text-white rounded p-2' 
+     onClick={()=>setCount(count+1)}>Increase</button>
     </div>
+
+
+     <input className='p-2 ml-8'
+      ref={inputRef} placeholder='Type Here....' />
+     <button onClick={()=>inputRef.current.focus()}
+      className='bg-sky-500 hover:bg-sky-700 text-xl text-white rounded p-2'>
+      Focus Input</button>
+     
+
+     </>
+
   )
 }
 
